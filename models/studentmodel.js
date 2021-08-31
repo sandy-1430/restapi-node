29420792ce305cfca3754aac4bd6e78997d7ded5
sandy-1430 <73515551+sandy-1430@users.noreply.github.com>
2021-08-31 22:05:20 +0530

    Update studentmodel.js

--- models/studentmodel.js
+++ models/studentmodel.js
@@ -4,7 +4,7 @@ const StudentSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     username: String,
     email: String,
-    phone: Number,
+    phone: String,
     gender: String,
     rollno: String,
     course: String,
