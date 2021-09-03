4f08df82f68d47eb1119fa74eb3239512579e6d0
sandy-1430 <73515551+sandy-1430@users.noreply.github.com>
2021-09-03 19:54:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
-
-
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         msg: "app is running"
-//     })
-// })
 
 const mangoose = require('mongoose');
 mangoose.connect('mongodb+srv://sandy:sandy1234@sandy.o8vni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
@@ -23,6 +15,7 @@ const studresultRouter = require('./router/studentresult');
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, OPTIONS, DELETE, POST, PUT");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
